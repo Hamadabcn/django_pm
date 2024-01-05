ed5b2620556040fc9133568a0bfbe1eb16bbd111
Hamada <triumphbcn@gmail.com>
2024-01-05 04:45:24 +0100

    Admin customization

--- projects/admin.py
+++ projects/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
+from django.db.models import Count
+
 from . import models
 
 admin.site.register(models.Category)
-admin.site.register(models.Project)
-admin.site.register(models.Task)
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'user', 'category', 'created_at', 'tasks_count')
+    list_per_page = 20
+    list_editable = ['status', 'user', 'category']
+    list_select_related = ['category', 'user']
+
+    def tasks_count(self, obj):
+         #return obj.task_set.count()
+        return obj.tasks_count
+
+    def get_queryset(self, request):
+        query = super().get_queryset(request)
+        query = query.annotate(tasks_count=Count('task'))
+        return query
+
+
+@admin.register(models.Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'description', 'project', 'is_completed')
+    list_editable = ['project', 'is_completed']
+    list_per_page = 20
