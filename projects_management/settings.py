39214f870fb38da36e6c4056d1f21988ee376093
Hamada <triumphbcn@gmail.com>
2024-01-05 17:44:52 +0100

    update staticfiles

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -132,8 +132,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
