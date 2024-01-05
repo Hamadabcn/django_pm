ff3abafcc865cb5306da606d08b9fb9f9595484b
Hamada <triumphbcn@gmail.com>
2024-01-05 17:45:34 +0100

    update staticfiles

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
