fbbec6a93f1da591173d1c332afee08097f24158
Hamada <triumphbcn@gmail.com>
2024-01-06 13:29:52 +0100

    fixed the logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from accounts.forms import UserLoginForm
 from projects.views import RegisterView, edit_profile
 
 urlpatterns = [
     path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', edit_profile, name='profile'),
     path('', include('django.contrib.auth.urls')),
